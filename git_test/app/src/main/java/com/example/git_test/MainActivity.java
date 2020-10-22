b28e5d5afe19f07af49f63f98cbcb27aa4328aca
Dangwuxie <974079012@qq.com>
2020-10-22 13:56:43 +0800

    测试代码

---
+++ git_test/app/src/main/java/com/example/git_test/MainActivity.java
@@ -0,0 +1,32 @@
+package com.example.git_test;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+
+
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+        System.out.println("这是一段测试代码");
+    }
+}
